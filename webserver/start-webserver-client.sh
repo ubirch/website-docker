8b2690ec1e26e558532cd5178d125ac158d5bd26
Falko Zurell <falko.zurell@here.com>
2016-02-25 21:53:14 +0100

    updated for new environment

--- webserver/start-webserver-client.sh
+++ webserver/start-webserver-client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 
-docker run --rm -ti --volumes-from webserver-data -v `readlink -f $1`:/mnt/backup maxheadroom/webserver /bin/bash
+docker run --rm -ti --volumes-from webserver-data -v `readlink -f $1`:/mnt/backup ubirch/webserver /bin/bash
--- webserver/start-webserver.sh
+++ webserver/start-webserver.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
 
 
-# docker run --name webserver -p 8080:80 --rm --volumes-from webserver-data --link mysql:mysql -v ${PWD}:/mnt/backup maxheadroom/webserver
-docker run --name webserver -p 8080:80 --rm -v `readlink -f $1`:/var/www/html --link mysql:mysql -v ${PWD}:/mnt/backup maxheadroom/webserver
+docker run --name webserver -p 8080:80 --rm -v `readlink -f $1`:/var/www/html --link mysql:mysql -v ${PWD}:/mnt/backup ubirch/webserver
